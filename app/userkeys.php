1947dba32d56bea9818a032b4789ae170996bce8
Robert Ian Hawdon <ian.hawdon@gmail.com>
2014-10-19 18:10:18 +0100

    another test

--- app/userkeys.php
+++ app/userkeys.php
@@ -8,13 +8,13 @@ $result = $con->query("CALL get_access_keys('$userid')") or die("Error: ".mysqli
 
 while($row = mysqli_fetch_array($result))
   {
-  $kkeyID = $row['ID'];
-  $kuserID = $row['user_ID'];
-  $ktoken = $row['token'];
-  $kcreated = $row['created'];
-  $kexpiry = $row['expiry'];
-  $kterminated = $row['terminated'];
-  $kcomment = $row['comment'];
+  $kkeyID[] = $row['ID'];
+  $kuserID[] = $row['user_ID'];
+  $ktoken[] = $row['token'];
+  $kcreated[] = $row['created'];
+  $kexpiry[] = $row['expiry'];
+  $kterminated[] = $row['terminated'];
+  $kcomment[] = $row['comment'];
   }
 
 #foreach ( $kkeyID as $index => $keyID ) {
